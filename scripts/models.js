f971871221e1d44149ae7764750ade074d6f0aae
RomZesM <dwarf.md@gmail.com>
2023-10-14 22:11:39 +0200

    feat: prototype of fetching json from server

---
+++ scripts/models.js
@@ -0,0 +1,6 @@
+export  class Objective{
+	constructor({objectiveId, name}){
+		this.objectiveId = objectiveId;
+		this.name = name;
+	}
+}
--- scripts/start.js
+++ scripts/start.js
@@ -1,1 +1,39 @@
+import { Objective } from "./models.js";
+
 console.log("HEll0 W0RLD!!!");
+
+
+
+const objectivesList = document.querySelector(".objectives");
+
+
+
+//get JSON from API
+async function getObjectivesFromServer(url) {
+	const res = await fetch('http://localhost:8080/api/objective/index');
+	const data = await res.json();
+		
+	getObjectivesFromJSON(data);
+  }
+//make instances from JSON
+function getObjectivesFromJSON(dataWithJson){
+	let objectiveArr = [];
+
+	for (let i = 0; i < dataWithJson.length; i++) {
+		const element = dataWithJson[i];
+		let objective = new Objective(JSON.parse(JSON.stringify(element)))
+		objectiveArr[i] = objective
+	}
+	
+	objectiveArr.forEach((element) => {
+		console.log("inside");
+		let listElement = document.createElement("li");
+		listElement.append(`Name - ${element.name} `);
+		listElement.append(`, id - ${element.objectiveId}.`);
+		listElement.appendChild(document.createElement("strong")).textContent = element.name;
+		objectivesList.appendChild(listElement);
+	});
+}
+
+getObjectivesFromServer();
+ 
